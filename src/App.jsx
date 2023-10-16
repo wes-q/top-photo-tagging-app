0c85b4651417ea18f92e0449b5346548ec0cd010
iamwesofph <piuswesley@gmail.com>
2023-10-16 10:37:50 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+function App() {
+    const [count, setCount] = useState(0);
+
+    return (
+        <>
+            <h1 className="text-3xl font-bold underline text-cyan-300">$$$ TEST</h1>
+        </>
+    );
+}
+
+export default App;
